579217a8fbf88c449a310b9d33fb4d9eb9f43d09
jurassicplayer <jurassicplayer75@gmail.com>
2023-01-09 21:59:46 -0800

    Fix unclean dismount and inc batt res to +-0.02%

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@ import {
   ServerAPI,
   staticClasses
 } from "decky-frontend-lib"
-import { BatteryState } from "./lib/SteamClient"
 import { FaBatteryQuarter } from "react-icons/fa"
 import { QAMPanel } from "./QAM/QAMPanel"
 import { Settings } from "./Utils/Settings"
 import { Backend } from "./Utils/Backend"
 import { SteamUtils } from "./Utils/SteamUtils"
+import { events } from "./Utils/Events"
+
+
 
 
 export default definePlugin((serverApi: ServerAPI) => {
@@ -17,36 +19,45 @@ export default definePlugin((serverApi: ServerAPI) => {
 
   let warnNotifiedState = false
   let criticalNotifiedState = false
+  let offset = 0.5
+  let resolution = 0.02
 
   // percentage check loop
-  SteamClient.System.RegisterForBatteryStateChanges((batteryState: BatteryState)=> {
-    let batteryPercent = Math.round(batteryState.flLevel * 100)
-    if (!criticalNotifiedState && batteryPercent < Settings.criticalLevel ) {
+  const IntervalCheck = (e: Event) => {
+    if (!Backend.getAppInitialized()) return
+    let batteryState = (e as events.BatteryStateEvent).batteryState
+    let batteryPercent = Math.round(batteryState.flLevel * 10000) / 100
+    if (!criticalNotifiedState && batteryPercent <= (Settings.criticalLevel+offset - resolution) ) {
       console.debug(`[AutoSuspend] Critical threshold triggered, current state: warnNotifiedState:${warnNotifiedState}, criticalNotifiedState:${criticalNotifiedState}, warnThreshold:${Settings.warningLevel}, critThreshold:${Settings.criticalLevel}, battPercent:${batteryPercent}, battRaw:${batteryState.flLevel}`)
       console.debug(batteryState)
       SteamUtils.notify("AutoSuspend", "Critical limit exceeded, suspending device", Settings.notificationEnabled, Settings.soundEnabled, 5000)
       setTimeout(() => {SteamUtils.suspend();}, 5500)
       criticalNotifiedState = true
-    } else if (!warnNotifiedState && batteryPercent < Settings.warningLevel && Settings.warningLevel > Settings.criticalLevel && !criticalNotifiedState) {
+    } else if (!warnNotifiedState && batteryPercent <= (Settings.warningLevel+offset - resolution) && Settings.warningLevel > Settings.criticalLevel && !criticalNotifiedState) {
       console.debug(`[AutoSuspend] Warning threshold triggered, current state: warnNotifiedState:${warnNotifiedState}, criticalNotifiedState:${criticalNotifiedState}, warnThreshold:${Settings.warningLevel}, critThreshold:${Settings.criticalLevel}, battPercent:${batteryPercent}, battRaw:${batteryState.flLevel}`)
       console.debug(batteryState)
       SteamUtils.notify("AutoSuspend", "Warning limit exceeded")
       warnNotifiedState = true
     }
-    if (criticalNotifiedState && batteryPercent > Settings.criticalLevel) {
+    if (criticalNotifiedState && batteryPercent > (Settings.criticalLevel+offset + resolution)) {
       console.debug(`[AutoSuspend] Reset criticalNotifiedState, current state: warnNotifiedState:${warnNotifiedState}, criticalNotifiedState:${criticalNotifiedState}, warnThreshold:${Settings.warningLevel}, critThreshold:${Settings.criticalLevel}, battPercent:${batteryPercent}, battRaw:${batteryState.flLevel}`)
       criticalNotifiedState = false
     }
-    if (warnNotifiedState && batteryPercent > Settings.warningLevel) {
+    if (warnNotifiedState && batteryPercent > (Settings.warningLevel+offset + resolution)) {
       console.debug(`[AutoSuspend] Reset warnNotifiedState, current state: warnNotifiedState:${warnNotifiedState}, criticalNotifiedState:${criticalNotifiedState}, warnThreshold:${Settings.warningLevel}, critThreshold:${Settings.criticalLevel}, battPercent:${batteryPercent}, battRaw:${batteryState.flLevel}`)
       warnNotifiedState = false
     }
-  })
+  }
+  Backend.eventBus.addEventListener(events.BatteryStateEvent.eType, IntervalCheck)
   Backend.setAppInitialized(true)
 
   return {
     title: <div className={staticClasses.Title}>AutoSuspend</div>,
     content: <QAMPanel />,
     icon: <FaBatteryQuarter />,
+    onDismount: () => {
+      Backend.eventBus.removeEventListener(events.BatteryStateEvent.eType, IntervalCheck)
+      Backend.onDismount()
+    }
   }
 })
