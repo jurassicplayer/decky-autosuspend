ca2fdf9090b3cbf7335bd44baad2c75793dfcc78
jurassicplayer <jurassicplayer75@gmail.com>
2023-06-25 05:59:09 -0700

    Add more alarm settings

--- src/Browser/AlarmItem.tsx
+++ src/Browser/AlarmItem.tsx
@@ -1,10 +1,11 @@
 import { FaBatteryFull, FaBatteryQuarter, FaBed, FaExclamationCircle, FaMoon, FaPowerOff, FaStopwatch, FaSun, FaUser, FaUsers, FaVolumeMute, FaVolumeUp } from "react-icons/fa"
+import { FaArrowRotateRight } from "react-icons/fa6"
 import { BiNotification, BiNotificationOff } from "react-icons/bi"
 import { IconContext } from "react-icons"
-import { DialogButton, DialogCheckbox, Dropdown, DropdownOption, ReorderableEntry, ToggleField } from "decky-frontend-lib"
-import { thresholdTypes, triggerActions } from "../Utils/Alarms"
+import { DialogButton, DropdownItem, ReorderableEntry, TextField, ToggleField } from "decky-frontend-lib"
+import { applyDefaults, thresholdTypes, triggerActions } from "../Utils/Alarms"
 import { useState, CSSProperties, SVGProps, useEffect } from "react"
-import { SteamCssVariables } from "../Utils/SteamUtils"
+import { NavSoundMap, SteamCssVariables, SteamUtils } from "../Utils/SteamUtils"
 import { useSettingsContext } from "../Utils/Context"
 import { thresholdLevelDefaults } from "../Utils/Settings"
 
@@ -59,7 +60,7 @@ const SteamCSS: {[key:string]: CSSProperties} = {
   },
   NotificationPrefDetails : {
     display: "flex",
-    flexDirection: "row",
+    flexDirection: "column",
     backgroundColor: SteamCssVariables.gpSystemDarkerGrey,
     padding: "1ex 0 0 5ex",
     boxShadow: "inset 0px 2px 3px rgba(0,0,0,.5)",
@@ -70,13 +71,7 @@ const SteamCSS: {[key:string]: CSSProperties} = {
 export const AlarmItem = (props: AlarmItemProps<EntryProps>) => {
   let alarmID = props.entry.data!.alarmID
   let { getSettings, getAlarmSettings, setAlarmSetting } = useSettingsContext()
-  let { defaultShowToast, defaultPlaySound, defaultSound, defaultAlarmRepeat, defaultAlarmType } = getSettings()
-  let { enabled, alarmName, profile, showToast, playSound, thresholdType, triggeredAction } = getAlarmSettings(alarmID)
-  let showToastOrDefault = (typeof showToast   != 'undefined') ? showToast    : defaultShowToast
-  let playSoundOrDefault = (typeof playSound   != 'undefined') ? playSound    : defaultPlaySound
-  //let soundOrDefault = (typeof sound       != 'undefined') ? sound        : defaultSound
-  //let alarmTypeOrDefault = (typeof alarmType   != 'undefined') ? alarmType    : defaultAlarmType
-  //let alarmRepeatOrDefault = (typeof alarmRepeat != 'undefined') ? alarmRepeat  : defaultAlarmRepeat
+  let { enabled, alarmName, profile, showToast, playSound, alarmRepeat, thresholdType, triggeredAction } = applyDefaults(getAlarmSettings(alarmID), getSettings())
   let [selected, setSelected] = useState<boolean>(false)
   let [loginUsers, setLoginUsers] = useState<LoginUser[]>([])
   let [profileData, setProfileData] = useState<ProfileData>()
@@ -144,8 +139,9 @@ export const AlarmItem = (props: AlarmItemProps<EntryProps>) => {
                 <FaUsers/><span style={{ fontSize: "0.6em", marginLeft: "0.7em" }}>Global</span>
               </div>
             }
-            {showToastOrDefault ? <BiNotification/> : <BiNotificationOff/>}
-            {playSoundOrDefault ? <FaVolumeUp/> : <FaVolumeMute/>}
+            {showToast ? <BiNotification/> : <BiNotificationOff/>}
+            {playSound ? <FaVolumeUp/> : <FaVolumeMute/>}
+            {alarmRepeat ? <FaArrowRotateRight/> : null}
             {thresholdTypeIcon}
             {triggerActionIcon}
           </div>
@@ -178,31 +174,90 @@ interface LoginUser extends ProfileData{
   rememberPassword: boolean
 }
 const AlarmItemSettings = (props: AlarmItemSettingsProps) => {
-  let { getAlarmSettings, setAlarmSettings, setAlarmSetting, deleteAlarmSetting } = useSettingsContext()
-  let { enabled, showToast, playSound, sound, alarmName, alarmMessage, alarmType, alarmRepeat, thresholdLevel, thresholdType, triggeredAction, profile, sortOrder } = getAlarmSettings(props.alarmID)
+  let { getSettings, getAlarmSettings, setAlarmSettings, setAlarmSetting, deleteAlarmSetting } = useSettingsContext()
+  let { enabled, showToast, playSound, sound, alarmName, alarmMessage, repeatToast, repeatSound, alarmRepeat, thresholdLevel, thresholdType, triggeredAction, profile, sortOrder } = applyDefaults(getAlarmSettings(props.alarmID), getSettings())
   let loginUsers: {label: string, data: LoginUser | null}[] = props.loginUsers.map((userData: LoginUser) => {
     return {label: userData.personaName, data: userData}
   })
   loginUsers.unshift({label: "Global", data: null})
   return (
     <div style={SteamCSS.NotificationPrefDetails}>
-      <DialogCheckbox
-        onChange={(value) => setAlarmSetting(props.alarmID, 'showToast', value)}
-        label="Toast"
-        description="Show toast notification"
-        disabled={!enabled}
-        bottomSeparator="none"
-        checked={showToast}
-      />
-      <DialogCheckbox
-        onChange={(value) => setAlarmSetting(props.alarmID, 'playSound', value)}
-        label="Sound"
-        description="Play toast sound"
-        disabled={!enabled}
-        bottomSeparator="none"
-        checked={playSound}
-      />
-      <Dropdown
+      <div style={{display: "flex", flexDirection: "row"}}>
+        <ToggleField
+          onChange={(value) => setAlarmSetting(props.alarmID, 'showToast', value)}
+          label="Toast"
+          description="Show toast notification"
+          disabled={!enabled}
+          bottomSeparator="none"
+          checked={showToast}
+        />
+        <ToggleField
+          onChange={(value) => setAlarmSetting(props.alarmID, 'playSound', value)}
+          label="Sound"
+          description="Play toast sound"
+          disabled={!enabled}
+          bottomSeparator="none"
+          checked={playSound}
+        />
+        <DropdownItem
+          rgOptions={(()=>{
+            let options = []
+            for (let key in NavSoundMap) {
+              let sfx = NavSoundMap[key]
+              if (typeof sfx != 'string') { continue }
+              options.push({
+                label: sfx.replace(/([A-Z])/g, ' $1').trim(),
+                data: sfx
+              })
+            }
+            return options
+          })()}
+          selectedOption={sound}
+          onChange={(value)=> {
+            setAlarmSetting(props.alarmID, 'sound', value.data)
+          }}
+          disabled={!enabled}
+          label={<span>Notification Sound:<span style={{fontSize: "0.8em", marginLeft: "0.8em"}}>Sound effect to use when alarm triggered</span></span>}
+          menuLabel="Notification Sound"
+          strDefaultLabel="Error"
+          focusable={enabled} />
+      </div>
+      <div style={{display: "flex", flexDirection: "row"}}>
+        <ToggleField
+            onChange={(value) => setAlarmSetting(props.alarmID, 'repeatToast', value)}
+            label="Repeat Toast"
+            description="Repeat toast notification"
+            disabled={!enabled}
+            bottomSeparator="none"
+            checked={repeatToast}
+          />
+        <ToggleField
+          onChange={(value) => setAlarmSetting(props.alarmID, 'repeatSound', value)}
+          label="Repeat Sound"
+          description="Repeat sound notification"
+          disabled={!enabled}
+          bottomSeparator="none"
+          checked={repeatSound}
+        />
+        <DropdownItem
+          rgOptions={(()=>{
+            let options = []
+            for (let i=0; i <= 9; i++) {
+              options.push({label: i.toString(), data: i})
+            }
+            return options
+          })()}
+          selectedOption={alarmRepeat || 0}
+          onChange={(value)=> {
+            setAlarmSetting(props.alarmID, 'alarmRepeat', value.data)
+          }}
+          disabled={!enabled}
+          label={<span>Repeat notification:<span style={{fontSize: "0.8em", marginLeft: "0.8em"}}>Number of times to repeat notification</span></span>}
+          menuLabel="Repeat Notification"
+          strDefaultLabel="Error"
+          focusable={enabled} />
+      </div>
+      <DropdownItem
         rgOptions={[
           {label: 'Discharge',        data: thresholdTypes.discharge},
           {label: 'Overcharge',       data: thresholdTypes.overcharge},
@@ -211,17 +266,28 @@ const AlarmItemSettings = (props: AlarmItemSettingsProps) => {
           {label: 'Session Playtime', data: thresholdTypes.sessionPlaytime}
         ]}
         selectedOption={thresholdType}
-        onChange={(value)=> {
+        onChange={(value) => {
           let alarmSettings = getAlarmSettings(props.alarmID)
           alarmSettings.thresholdType = value.data
           alarmSettings.thresholdLevel = thresholdLevelDefaults[value.data]
           setAlarmSettings(props.alarmID, alarmSettings)
         }}
         disabled={!enabled}
+        label={<span>Threshold Type:<span style={{fontSize: "0.8em", marginLeft: "0.8em"}}>Type of threshold to trigger alarm</span></span>}
         menuLabel="Threshold Type"
         strDefaultLabel="Error"
         focusable={enabled} />
-      <Dropdown
+      <div>
+        <DropdownItem
+          rgOptions={[
+            {label: '', data: ''}
+          ]}
+          selectedOption={''}
+          onChange={(value) => {
+            console.log(value)
+          }}/>
+      </div>
+      <DropdownItem
         rgOptions={[
           {label: 'None',     data: triggerActions.none},
           {label: 'Suspend',  data: triggerActions.suspend},
@@ -230,17 +296,38 @@ const AlarmItemSettings = (props: AlarmItemSettingsProps) => {
         selectedOption={triggeredAction}
         onChange={(value)=> setAlarmSetting(props.alarmID, 'triggeredAction', value.data)}
         disabled={!enabled}
+        label={<span>Trigger Action:<span style={{fontSize: "0.8em", marginLeft: "0.8em"}}>Action to perform when alarm triggered</span></span>}
         menuLabel="Trigger Action"
         strDefaultLabel="Error"
         focusable={enabled} />
-      <Dropdown
+      <DropdownItem
         rgOptions={loginUsers}
         selectedOption={loginUsers.find((user)=>user.data && user.data.accountName == profile)?.data || null}
         onChange={(value)=> value.data ? setAlarmSetting(props.alarmID, 'profile', value.data.accountName): deleteAlarmSetting(props.alarmID, 'profile') }
         disabled={!enabled}
+        label={<span>Profile:<span style={{fontSize: "0.8em", marginLeft: "0.8em"}}>Apply this alarm only for the specified Steam user</span></span>}
         menuLabel="Per Profile"
         strDefaultLabel="Error"
         focusable={enabled} />
+      <TextField
+        label="Alarm Name"
+        description="Name of the alarm"
+        bShowClearAction={true}
+        value={alarmName}
+        onChange={(e)=> setAlarmSetting(props.alarmID, 'alarmName', e.target.value)} />
+      <TextField
+        label="Alarm Message"
+        description="Message displayed on alarm toast"
+        bShowClearAction={true}
+        value={alarmMessage}
+        onChange={(e)=> {
+          if (e.target.value != '') {
+            setAlarmSetting(props.alarmID, 'alarmMessage', e.target.value)
+          } else {
+            deleteAlarmSetting(props.alarmID, 'alarmMessage')
+          }
+        }} />
+      <DialogButton onOKButton={()=>{SteamUtils.notify(alarmName, alarmMessage, showToast, playSound, sound)}}>Test Notification</DialogButton>
       <DialogButton onOKButton={()=>{console.log(getAlarmSettings(props.alarmID))}}>Context Settings</DialogButton>
     </div>
   )
