a35e1ee11e708e63a73d73ab695b10ff093e832e
jurassicplayer <jurassicplayer75@gmail.com>
2023-06-30 04:42:03 -0700

    Partial implementation of threshold level

--- src/Browser/AlarmSettings.tsx
+++ src/Browser/AlarmSettings.tsx
@@ -1,13 +1,33 @@
-import { DialogButton, DropdownItem, TextField, ToggleField } from "decky-frontend-lib"
-import { AlarmItemSettingsProps, LoginUser, thresholdTypes, triggerActions } from "../Utils/Interfaces"
+import { DialogButton, DropdownItem, NotchLabel, SliderField, TextField, ToggleField } from "decky-frontend-lib"
+import { AlarmItemSettingsProps, LoginUser, thresholdLevels, thresholdTypes, triggerActions } from "../Utils/Interfaces"
 import { NavSoundMap, SteamCss, SteamUtils } from "../Utils/SteamUtils"
 import { useSettingsContext } from "../Utils/Context"
 import { applyDefaults, thresholdLevelDefaults } from "../Utils/Settings"
-import { CSSProperties } from "react"
+import { CSSProperties, useState } from "react"
 
 const descriptionCss: CSSProperties = {
   fontSize: "0.8em",
   marginLeft: "0.8em"
+}
+
+const applyThresholdValue = (thresholdLevels: thresholdLevels, thresholdType: thresholdTypes, value: number): thresholdLevels => {
+  switch (thresholdType) {
+    case thresholdTypes.discharge: thresholdLevels.discharge = value; break
+    case thresholdTypes.overcharge: thresholdLevels.overcharge = value; break
+    case thresholdTypes.bedtime: thresholdLevels.bedtime = value; break
+    case thresholdTypes.dailyPlaytime: thresholdLevels.dailyPlaytime = value; break
+    case thresholdTypes.sessionPlaytime: thresholdLevels.sessionPlaytime = value; break
+  }
+  return thresholdLevels
+}
+const getThresholdValue = (thresholdLevels: thresholdLevels, thresholdType: thresholdTypes): number => {
+  switch (thresholdType) {
+    case thresholdTypes.discharge: return thresholdLevels.discharge
+    case thresholdTypes.overcharge: return thresholdLevels.overcharge
+    case thresholdTypes.bedtime: return thresholdLevels.bedtime
+    case thresholdTypes.dailyPlaytime: return thresholdLevels.dailyPlaytime
+    case thresholdTypes.sessionPlaytime: return thresholdLevels.sessionPlaytime
+  }
 }
 
 export const AlarmItemSettings = (props: AlarmItemSettingsProps) => {
@@ -17,6 +37,7 @@ export const AlarmItemSettings = (props: AlarmItemSettingsProps) => {
     return {label: userData.personaName, data: userData}
   })
   loginUsers.unshift({label: "Global", data: null})
+  let [componentState, setComponentState] = useState<thresholdLevels>(applyThresholdValue(thresholdLevelDefaults, thresholdType, thresholdLevel))
   return (
     <div style={{...SteamCss.NotificationPrefDetails, padding: "1ex 1ex 0 2ex"}}>
       <div style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}>
@@ -62,19 +83,51 @@ export const AlarmItemSettings = (props: AlarmItemSettingsProps) => {
         strDefaultLabel="Error"
         focusable={!enabled} />
       <div>
-        <DropdownItem
-          rgOptions={[
-            {label: '', data: ''}
-          ]}
-          selectedOption={''}
-          onChange={(value) => {
-            console.log(value)
-          }}
-          disabled={enabled}
-          label={<span>Threshold Level:<span style={descriptionCss}>Type of threshold to trigger alarm</span></span>}
-          bottomSeparator="none"
-          strDefaultLabel="Error"
-          focusable={!enabled}/>
+        { !enabled && (thresholdType === thresholdTypes.discharge || thresholdType === thresholdTypes.overcharge) ? 
+          <SliderField
+            label="Threshold Level"
+            description="Add thy threshold level description here ##FIXME##"
+            value={getThresholdValue(componentState, thresholdType)}
+            min={0}
+            max={100}
+            step={1}
+            notchCount={5}
+            notchLabels={(()=>{
+              let labels:NotchLabel[] = []
+              let sliderMax = 100
+              let divisor = 4
+              for (let i=0;i<11;i++) {
+                labels.push({notchIndex: i, label: (i*(sliderMax/divisor)).toString()})
+              }
+              return labels
+            })()}
+            showValue={true}
+            disabled={enabled}
+            editableValue={true}
+            validValues='range'
+            valueSuffix="%"
+            onChange={(value)=>{
+              setAlarmSetting(props.alarmID, 'thresholdLevel', value)
+              let state = applyThresholdValue(componentState, thresholdType, value)
+              setComponentState(state)
+            }}
+            />
+        : null}
+        { thresholdType === thresholdTypes.bedtime || thresholdType === thresholdTypes.dailyPlaytime || thresholdType === thresholdTypes.sessionPlaytime ?
+          <DropdownItem
+            rgOptions={[{label: '', data: ''}]}
+            selectedOption={''}
+            label="Threshold Level"
+            description="Add thy threshold level description here ##FIXME##"
+            disabled={enabled}
+            focusable={!enabled}/>
+        : null}
+        { enabled && (thresholdType === thresholdTypes.discharge || thresholdType === thresholdTypes.overcharge) ? 
+          <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
+            <span>Threshold Level: <span>Add thy threshold level description here ##FIXME##</span></span>
+            <span>{getThresholdValue(componentState, thresholdType)}</span>
+          </div>
+        : null}
       </div>
       <DropdownItem
         rgOptions={[
@@ -97,41 +150,39 @@ export const AlarmItemSettings = (props: AlarmItemSettingsProps) => {
         bottomSeparator="none"
         strDefaultLabel="Error"/>
       <h3>Notification</h3>
-      {/* <div style={{display: "flex", flexDirection: "column"}}> */}
-        <ToggleField
-          onChange={(value) => setAlarmSetting(props.alarmID, 'showToast', value)}
-          label="Toast"
-          description="Enable toast on notification"
-          bottomSeparator="none"
-          checked={showToast} />
-        <ToggleField
-          onChange={(value) => setAlarmSetting(props.alarmID, 'playSound', value)}
-          label="Sound"
-          description="Enable sound on notification"
-          bottomSeparator="none"
-          checked={playSound} />
-        <DropdownItem
-          rgOptions={(()=>{
-            let options = []
-            for (let key in NavSoundMap) {
-              let sfx = NavSoundMap[key]
-              if (typeof sfx != 'string') { continue }
-              options.push({
-                label: sfx.replace(/([A-Z])/g, ' $1').trim(),
-                data: sfx
-              })
-            }
-            return options
-          })()}
-          selectedOption={sound}
-          onChange={(value)=> { setAlarmSetting(props.alarmID, 'sound', value.data) }}
-          label={<span>Notification Sound:<span style={descriptionCss}>SteamOS navigation sound effect to play on notification</span></span>}
-          bottomSeparator="none"
-          strDefaultLabel="Error" />
-        <DialogButton
-          style={{minWidth: "0px", width: "10em", alignSelf: "flex-end"}}
-          onOKButton={()=>{SteamUtils.notify(alarmName, alarmMessage, showToast, playSound, sound)}}>Test</DialogButton>
-      {/* </div> */}
+      <ToggleField
+        onChange={(value) => setAlarmSetting(props.alarmID, 'showToast', value)}
+        label="Toast"
+        description="Enable toast on notification"
+        bottomSeparator="none"
+        checked={showToast} />
+      <ToggleField
+        onChange={(value) => setAlarmSetting(props.alarmID, 'playSound', value)}
+        label="Sound"
+        description="Enable sound on notification"
+        bottomSeparator="none"
+        checked={playSound} />
+      <DropdownItem
+        rgOptions={(()=>{
+          let options = []
+          for (let key in NavSoundMap) {
+            let sfx = NavSoundMap[key]
+            if (typeof sfx != 'string') { continue }
+            options.push({
+              label: sfx.replace(/([A-Z])/g, ' $1').trim(),
+              data: sfx
+            })
+          }
+          return options
+        })()}
+        selectedOption={sound}
+        onChange={(value)=> { setAlarmSetting(props.alarmID, 'sound', value.data) }}
+        label={<span>Notification Sound:<span style={descriptionCss}>SteamOS navigation sound effect to play on notification</span></span>}
+        bottomSeparator="none"
+        strDefaultLabel="Error" />
+      <DialogButton
+        style={{minWidth: "0px", width: "10em", alignSelf: "flex-end"}}
+        onOKButton={()=>{SteamUtils.notify(alarmName, alarmMessage, showToast, playSound, sound, 1000)}}>Test</DialogButton>
       { getSetting('debuggingMode') ?
       <div style={{display: "flex", flexDirection: "row"}}>
         <ToggleField
