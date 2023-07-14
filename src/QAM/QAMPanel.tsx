debc77e7e4b662919aaa3a9ed75a5ab88cbc4680
jurassicplayer <jurassicplayer75@gmail.com>
2023-07-14 13:04:43 -0700

    Add placeholder buttons to other pages on QAM

--- src/QAM/QAMPanel.tsx
+++ src/QAM/QAMPanel.tsx
@@ -4,16 +4,22 @@ import {
   Navigation
 } from "decky-frontend-lib"
 import { VFC } from "react"
+import { useAppContext } from "../Utils/Context"
 
-
-const NavigateToAlarms = () => {
-  Navigation.Navigate("/autosuspend/alarms")
-  Navigation.CloseSideMenus()
-}
 export const QAMPanel: VFC = () => {
+  const { activeRoutes } = useAppContext()
+  const NavigateToPage = (path:string) => {
+    if (!activeRoutes.includes(path)) { return }
+    Navigation.Navigate(path)
+    Navigation.CloseSideMenus()
+  }
+  
   return (
     <Focusable>
-      <DialogButton onOKButton={NavigateToAlarms}>Settings</DialogButton>
+      <DialogButton onClick={()=>NavigateToPage("/autosuspend/alarms")}>Alarm List</DialogButton>
+      <DialogButton onClick={()=>NavigateToPage("/autosuspend/settings")}>Global Settings</DialogButton>
+      <DialogButton onClick={()=>NavigateToPage("/autosuspend/information")}>Information</DialogButton>
+      <DialogButton onClick={()=>NavigateToPage("/autosuspend/about")}>About</DialogButton>
     </Focusable>
   )
 }
