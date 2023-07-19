a8364476b9156518cb5df4c8c213832fcb52d5f0
jurassicplayer <jurassicplayer75@gmail.com>
2023-07-18 18:50:42 -0700

    Update to use decky_plugin env variables

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python
 import logging, os
 from settings import SettingsManager # type: ignore
+import decky_plugin # type: ignore
 
 # Setup environment variables
-deckyHomeDir = os.environ["DECKY_HOME"]
-settingsDir = os.environ["DECKY_PLUGIN_SETTINGS_DIR"]
-loggingDir = os.environ["DECKY_PLUGIN_LOG_DIR"]
+deckyHomeDir = decky_plugin.DECKY_HOME
+settingsDir = decky_plugin.DECKY_PLUGIN_SETTINGS_DIR
+loggingDir = decky_plugin.DECKY_PLUGIN_LOG_DIR
+logger = decky_plugin.logger
 
 # Setup backend logger
-logging.basicConfig(filename=os.path.join(loggingDir, 'backend.log'),
-                    format='%(asctime)s %(levelname)s %(message)s',
-                    filemode='w+',
-                    force=True)
-logger=logging.getLogger()
 logger.setLevel(logging.DEBUG) # can be changed to logging.DEBUG for debugging issues
-
 logger.info('[backend] Settings path: {}'.format(settingsDir))
 settings = SettingsManager(name="settings", settings_directory=settingsDir)
 settings.read()
