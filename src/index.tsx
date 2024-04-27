016220e805739cbb0e31d40fd6c987ef311c165b
jurassicplayer <jurassicplayer75@gmail.com>
2024-04-27 03:55:22 -0700

    Use single app context

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,24 @@
 import { definePlugin, ServerAPI, staticClasses } from "decky-frontend-lib"
 import { FaBatteryQuarter } from "react-icons/fa"
-//import { AppContextProvider, AppContextState } from "./Plugin/Context"
 import QAM from "./Views/QAM"
 //import PageRouter from "./Views/PageRouter"
 
-import { Backend } from "./Plugin/BackendContext"
-import { SettingsContextProvider } from "./Plugin/SettingsContext"
-import { AppContext, AppContextProvider } from "./Plugin/AppContext"
+import { Backend } from "./Plugin/Backend"
+import { AppContextProvider, AppContextState } from "./Plugin/AppContext"
 
 export default definePlugin((serverAPI: ServerAPI) => {
   Backend.initialize(serverAPI)
-  let appContext = new AppContext()
   
   return {
     title: <div className={staticClasses.Title}>AutoSuspend</div>,
     content: (
-      <SettingsContextProvider>
-        <AppContextProvider appContext={appContext}>
-          <QAM/>
-        </AppContextProvider>
-      </SettingsContextProvider>
+      <AppContextProvider>
+        <QAM/>
+      </AppContextProvider>
     ),
     icon: <FaBatteryQuarter />,
-    alwaysRender: true,
     onDismount: () => {
-      appContext.onDismount()
+      new AppContextState().onDismount()
     }
   }
 })
