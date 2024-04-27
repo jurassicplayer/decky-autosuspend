2269b0c9b8a31302e21f544d252dc05bb53cdd77
jurassicplayer <jurassicplayer75@gmail.com>
2024-04-27 03:55:46 -0700

    Move functions elsewhere

--- src/Plugin/Hooks.ts
+++ src/Plugin/Hooks.ts
@@ -43,36 +43,36 @@ export function toHook(hookType: HookType): IHook {
   return hook
 }
 
-export function registerHook(hookType: HookType): IHook {
-  let hook: IHook = toHook(hookType)
-  let context = {}
-  switch (hookType) {
-    case HookType.RegisterForBatteryStateChangesPseudo:
-      // Pass context
-      hook.unregister = CreatePseudoBatteryStateRegistry(context)
-      break;
-    case HookType.RegisterForTimeStateChangePseudo:
-      // Pass context
-      hook.unregister = CreatePseudoBatteryStateRegistry(context)
-      break;
-    case HookType.RegisterForSettingsChanges:
-      //hook.unregister = SteamClient[hook.subsystem][hook.register]((evt: Event)=> { context.eventBus.dispatchEvent(new SettingsChangeEvent(events.map.SettingsChange, { detail: evt })) })
-      break;
-  }
-  return hook
-}
+// export function registerHook(hookType: HookType): IHook {
+//   let hook: IHook = toHook(hookType)
+//   let context = {}
+//   switch (hookType) {
+//     case HookType.RegisterForBatteryStateChangesPseudo:
+//       // Pass context
+//       hook.unregister = CreatePseudoBatteryStateChangeRegistry(context)
+//       break;
+//     case HookType.RegisterForTimeStateChangePseudo:
+//       // Pass context
+//       hook.unregister = CreatePseudoBatteryStateChangeRegistry(context)
+//       break;
+//     case HookType.RegisterForSettingsChanges:
+//       //hook.unregister = SteamClient[hook.subsystem][hook.register]((evt: Event)=> { context.eventBus.dispatchEvent(new SettingsChangeEvent(events.map.SettingsChange, { detail: evt })) })
+//       break;
+//   }
+//   return hook
+// }
 
-import { BatteryStateEvent } from "./../Alarms/Battery/Battery.interfaces"
-import { events } from "./BackendContext.h"
-function CreatePseudoBatteryStateRegistry(context: any) {
-  // Set context.batteryIntervalID
-  context.intervalID = setInterval(() => {
-    // @ts-ignore
-    let currentState = window.SystemPowerStore.batteryState
-    if (context.batteryState == currentState) return
-    context.batteryState = currentState
-    context.eventBus.dispatchEvent(new BatteryStateEvent(events.map.BatteryState, { detail: context.batteryState }))
-  }, 1000)
-  return () => clearInterval(context.intervalID)
+// import { BatteryStateChangeEvent } from "./../Alarms/Battery/Battery.interfaces"
+// import { events } from "./BackendContext.h"
+// function CreatePseudoBatteryStateChangeRegistry(context: any) {
+//   // Set context.batteryIntervalID
+//   context.intervalID = setInterval(() => {
+//     // @ts-ignore
+//     let currentState = window.SystemPowerStore.batteryState
+//     if (context.batteryState == currentState) return
+//     context.batteryState = currentState
+//     context.eventBus.dispatchEvent(new BatteryStateChangeEvent(events.map.BatteryStateChange, { detail: context.batteryState }))
+//   }, 1000)
+//   return () => clearInterval(context.intervalID)
 
-}
+// }
